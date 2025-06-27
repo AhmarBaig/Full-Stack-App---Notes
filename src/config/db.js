ca5c684d0bae54a75e02a618fb0b3cc855febd15
Ahmar Baig <54508892+AhmarBaig@users.noreply.github.com>
2025-06-27 14:02:27 -0400

    removing API key lol

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,13 @@
+/* eslint-disable no-undef */
 // Will connect to MongoDB
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://ahmarbaig2000:HvL2ub5mLDhOCurD@cluster0.pxw72io.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect(mongoURI)
     .then(() => {
         console.log('Connected to Database!')
     })
